7d53c6c89d912acc32734c88f312e5e1448a872b
HandBoy <handersonmedeiros@gmail.com>
2011-01-24 13:55:15 -0300

    iniciando um blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogHand::Application.config.secret_token = '748680ddf49ae0536876eab0bedba74c70dbb4ee5a650de03a417322e66722498c54ee3754f5fd6373ceda152569cfb2fb3ad9c5177a2b14b8a4cdea01d870c4'
